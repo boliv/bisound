9633958b424256a7801d5528b3e56dc55a0d1405
Thuong Ho <thuong.t.ho@gmail.com>
2017-02-11 21:26:47 -0800

    add updated list of music for testing

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -1,7 +1,11 @@
 var musicController = function () {
   var get = function (req,res) {
     var responseJson = {
-      sync_url: "http://someurl.com"
+      jay: "http://listen.radionomy.com/abc-lounge",
+      thuong: "http://listen.radionomy.com/bestclubdance",
+      jerome: "http://listen.radionomy.com/quisqueya",
+      kauna: "http://listen.radionomy.com/ledjamradio.mp3",
+      cyrus: "http://listen.radionomy.com/100-hit-radio"
     };
 
     res.json(responseJson);
